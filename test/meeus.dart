82161fb02d27bcccf8aec29c7a5239c948659c7f
Thysia.Zosa <thysia.zosa@gmail.com>
2021-09-26 17:24:05 +0200

    Finish test for transit etc.

--- test/meeus.dart
+++ test/meeus.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'dart:math';
 
 void main() {
@@ -13,6 +14,53 @@ void main() {
     return '$hours:$minutes:$seconds';
   }
 
+  double degToRad(num degree) => pi * degree / 180;
+
+  double degMinSecToRad(
+    int sign,
+    int degree,
+    int minute,
+    num second,
+  ) {
+    return sign * degToRad(degree + minute / 60 + second / 3600);
+  }
+
+  double secondsToRad(num seconds) => seconds * pi / 43200;
+
+  double timeToRad(
+    int hour,
+    int minute,
+    num second,
+  ) {
+    return secondsToRad(hour * 3600 + minute * 60 + second);
+  }
+
+  double radToDeg(num radians) => 180 * radians / pi;
+
+  double modRad(num radians) {
+    return (radians + 2 * pi) % (2 * pi);
+  }
+
+  /// interpolation:
+  /// y = y2 + n/2 * (a+b+nc)
+  /// y123: 1., 2., 3. Wert
+  /// n: Bruchteil
+  /// a: y2 - y1
+  /// b: y3 - y2
+  /// c: b - a
+  double interpolation({
+    required num firstTerm,
+    required num middleTerm,
+    required num thirdTerm,
+    required num part,
+  }) {
+    var a = middleTerm - firstTerm;
+    var b = thirdTerm - middleTerm;
+    var c = b - a;
+    var value = middleTerm + part * (a + b + part * c) / 2;
+    return value;
+  }
+
   /// sin(h) = sin(phi)sin(delta)+cos(phi)cos(delta)cos(H)
   /// h: 'Null'punkt, 5/6°
   /// phi: gegraphische Breite
@@ -21,7 +69,7 @@ void main() {
   /// cosH = (sin(h)-sin(phi)sin(delta))/(cos(phi)cos(delta))
   ///
   /// [heightAtHorizon], [latitude] und [declination] sind in Winkelmass (Radian) anzugeben
-  double getHourAngle({
+  double? getHourAngle({
     required num heightAtHorizon,
     required num latitude,
     required num declination,
@@ -29,7 +77,7 @@ void main() {
     var dividend = sin(heightAtHorizon) - sin(latitude) * sin(declination);
     var divisor = cos(latitude) * cos(declination);
     var cosHourAngle = dividend / divisor;
-    return acos(cosHourAngle);
+    return cosHourAngle.abs() > 1 ? null : acos(cosHourAngle);
   }
 
   // Julianisches Datum um 0h UT
@@ -69,11 +117,178 @@ void main() {
     return date.subtract(Duration(milliseconds: deltaT));
   }
 
+  // sin(h) = sin(d)sin(f)+cos(d)cos(f)cos(H)
+  // h: Höhe, Ziel der Berechnung
+  // f: geographische Breite, einzugeben
+  double getHeight(
+    num latitude,
+    num hourAngle,
+    num declination,
+  ) {
+    var sinDec = sin(declination);
+    var sinLat = sin(latitude);
+    var cosDec = cos(declination);
+    var cosLat = cos(latitude);
+    var cosHour = cos(hourAngle);
+    var sinHeight = sinDec * sinLat + cosDec * cosLat * cosHour;
+    var height = asin(sinHeight);
+    return height;
+  }
+
   var date = DateTime.utc(1988, 3, 20);
   var dtDate = getDtDate(date);
   var julianDate = getJulianDate(date);
-  var capThetaZero = getApSidTimeZero(julianDate);
+  var capThetaZero = secondsToRad(getApSidTimeZero(julianDate));
+  print('Θ0: ${radToDeg(capThetaZero)}');
   var julianDtDate = getJulianDate(dtDate);
   var sidTime = getApparentSidTime(julianDtDate);
-  print(secondsToTime(capThetaZero));
+  // print(secondsToTime(capThetaZero));
+
+  // L
+  var long = degMinSecToRad(1, 71, 5, 0);
+  print('L: ${radToDeg(long)}');
+  var lat = degMinSecToRad(1, 42, 20, 0);
+  print('φ: ${radToDeg(lat)}');
+  var a1 = timeToRad(2, 42, 43.25);
+  print('α1: ${radToDeg(a1)}');
+  var a2 = timeToRad(2, 46, 55.51);
+  print('α2: ${radToDeg(a2)}');
+  var a3 = timeToRad(2, 51, 7.69);
+  print('α3: ${radToDeg(a3)}');
+  var d1 = degMinSecToRad(1, 18, 2, 51.4);
+  print('δ1: ${radToDeg(d1)}');
+  var d2 = degMinSecToRad(1, 18, 26, 27.3);
+  print('δ2: ${radToDeg(d2)}');
+  var d3 = degMinSecToRad(1, 18, 49, 38.7);
+  print('δ3: ${radToDeg(d3)}');
+  var h0 = degMinSecToRad(-1, 0, 34, 0);
+  print('h0: ${radToDeg(h0)}');
+  var deltaT = 56;
+  var capH0 = getHourAngle(
+    heightAtHorizon: h0,
+    latitude: lat,
+    declination: d2,
+  );
+  if (capH0 == null) {
+    print('circumPolar');
+    exit(1);
+  }
+  print('H0: ${radToDeg(capH0)}');
+  // m0
+  var transit = (a2 + long - capThetaZero) / (2 * pi) % 1;
+  print('m0: $transit');
+  // m1
+  var rising = (transit - capH0 / (2 * pi)) % 1;
+  print('m1: $rising');
+  // m2
+  var setting = (transit + capH0 / (2 * pi)) % 1;
+  print('m2: $setting');
+
+  // --------------------------------------- transit
+  // θ0
+  var theta0 = modRad(capThetaZero + 2 * pi * transit);
+  print('θ0: ${radToDeg(theta0)}');
+  // n
+  var n = transit + deltaT / 86400;
+  print('n: $n');
+  // interpolated α
+  var a = interpolation(firstTerm: a1, middleTerm: a2, thirdTerm: a3, part: n);
+  print('int. α: ${radToDeg(a)}');
+  // // H
+  var capH = theta0 - long - a;
+  print('H: ${radToDeg(capH)}');
+  // Δm
+  var deltaM = -capH / (2 * pi);
+  print('Δm: $deltaM');
+  // corrected m
+  transit = transit + deltaM;
+  print('m0: $transit');
+
+  // Durchgang
+  theta0 = modRad(capThetaZero + 2 * pi * transit);
+  n = transit + deltaT / 86400;
+  a = interpolation(firstTerm: a1, middleTerm: a2, thirdTerm: a3, part: n);
+  capH = theta0 - long - a;
+  deltaM = -capH / (2 * pi);
+  transit = transit + deltaM;
+  print('m0: $transit');
+
+  // Durchgang
+  theta0 = modRad(capThetaZero + 2 * pi * transit);
+  n = transit + deltaT / 86400;
+  a = interpolation(firstTerm: a1, middleTerm: a2, thirdTerm: a3, part: n);
+  capH = theta0 - long - a;
+  deltaM = -capH / (2 * pi);
+  transit = transit + deltaM;
+  print('m0: $transit');
+
+  // Durchgang
+  theta0 = modRad(capThetaZero + 2 * pi * transit);
+  n = transit + deltaT / 86400;
+  a = interpolation(firstTerm: a1, middleTerm: a2, thirdTerm: a3, part: n);
+  capH = theta0 - long - a;
+  deltaM = -capH / (2 * pi);
+  transit = transit + deltaM;
+  print('m0: $transit');
+
+  // Durchgang
+  theta0 = modRad(capThetaZero + 2 * pi * transit);
+  n = transit + deltaT / 86400;
+  a = interpolation(firstTerm: a1, middleTerm: a2, thirdTerm: a3, part: n);
+  capH = theta0 - long - a;
+  deltaM = -capH / (2 * pi);
+  transit = transit + deltaM;
+  print('m0: $transit');
+
+  // // --------------------------------------- rising
+  // // θ0
+  // theta0 = modRad(capThetaZero + 2 * pi * rising);
+  // print('θ0: ${radToDeg(theta0)}');
+  // // n
+  // n = rising + deltaT / 86400;
+  // print('n: $n');
+  // // interpolated α
+  // a = interpolation(firstTerm: a1, middleTerm: a2, thirdTerm: a3, part: n);
+  // print('int. α: ${radToDeg(a)}');
+  // // interpolated δ
+  // var d = interpolation(firstTerm: d1, middleTerm: d2, thirdTerm: d3, part: n);
+  // print('int. δ: ${radToDeg(d)}');
+  // // H
+  // capH = theta0 - long - a;
+  // print('H: ${radToDeg(capH)}');
+  // // h
+  // var h = getHeight(lat, capH, d);
+  // print('h: ${radToDeg(h)}');
+  // // Δm
+  // deltaM = (h - h0) / (cos(d) * cos(lat) * sin(capH));
+  // print('Δm: ${radToDeg(deltaM)}');
+  // // corrected m
+  // rising = rising + deltaM;
+  // print('m1: $rising');
+
+  // // --------------------------------------- setting
+  // // θ0
+  // theta0 = modRad(capThetaZero + 2 * pi * setting);
+  // print('θ0: ${radToDeg(theta0)}');
+  // // n
+  // n = setting + deltaT / 86400;
+  // print('n: $n');
+  // // interpolated α
+  // a = interpolation(firstTerm: a1, middleTerm: a2, thirdTerm: a3, part: n);
+  // print('int. α: ${radToDeg(a)}');
+  // // interpolated δ
+  // d = interpolation(firstTerm: d1, middleTerm: d2, thirdTerm: d3, part: n);
+  // print('int. δ: ${radToDeg(d)}');
+  // // H
+  // capH = theta0 - long - a;
+  // print('H: ${radToDeg(capH)}');
+  // // h
+  // h = getHeight(lat, capH, d);
+  // print('h: ${radToDeg(h)}');
+  // // Δm
+  // deltaM = (h - h0) / (cos(d) * cos(lat) * sin(capH));
+  // print('Δm: ${radToDeg(deltaM)}');
+  // // corrected m
+  // setting = setting + deltaM;
+  // print('m1: $setting');
 }
