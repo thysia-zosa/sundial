70fe2ff9099ecb6a517ac965ade61bce901293eb
Jonas Tochtermann <thysia.zosa@gmail.com>
2022-08-20 17:10:09 +0200

    feat: add data

---
+++ lib/src/gereedschappen/daten.dart
@@ -0,0 +1,47 @@
+Map<String, dynamic> talen = {
+  'nederlands': <String, dynamic>{
+    'weekdagen': <String>[
+      'Herendag',
+      'Tweedag',
+      'Driedag',
+      'Vierdag',
+      'Vijfdag',
+      'Kruisdag',
+      'Sabbat',
+      'Herendag',
+    ],
+    'maanden': <String>[
+      'Moeharram',
+      'Safar',
+      'Een Rabi',
+      'Twee Rabi',
+      'Een Djoemada',
+      'Twee Djoemada',
+      'Radjab',
+      'Sjaban',
+      'Ramadan',
+      'Sjauwal',
+      'Doelkada',
+      'Doelhidja',
+    ],
+    'groeten': <String>[
+      'Avondgebed',
+      'Middaggebed',
+      'Voormiddag',
+      'Morgengebed',
+      'Middernacht',
+      'Dagsluiting',
+      'Nachtgebed',
+    ]
+  },
+};
+
+List<int> tijden = [
+  22,
+  18,
+  15,
+  11,
+  2,
+  1,
+  0,
+];
