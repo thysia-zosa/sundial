54b966cbd1a0172af0e9bcde4aa8c2c9a99b2b02
Jonas Tochtermann <thysia.zosa@gmail.com>
2022-08-20 17:12:00 +0200

    feat: implement sundial

--- lib/src/schermen/thuis_scherm.dart
+++ lib/src/schermen/thuis_scherm.dart
@@ -1,7 +1,11 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:zonnewijzer/src/gereedschappen/daten.dart';
+import 'package:zonnewijzer/src/logica/kalender.dart';
 import 'dart:math' as math;
+
+import 'package:zonnewijzer/src/modellen/zonnetijd.dart';
 
 class ThuisScherm extends StatefulWidget {
   const ThuisScherm({Key? key}) : super(key: key);
@@ -12,24 +16,25 @@ class ThuisScherm extends StatefulWidget {
 }
 
 class _ThuisSchermState extends State<ThuisScherm> {
+  final Kalender kalender = Kalender();
   double _hours = 0;
   double _minutes = 0;
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _calculateTime();
-  }
+  // @override
+  // void initState() {
+  //   // TODO: implement initState
+  //   super.initState();
+  //   _calculateTime();
+  // }
 
-  void _calculateTime() {
-    DateTime now = DateTime.now();
-    setState(() {
-      _hours = now.hour + now.minute / 60;
-      _minutes = now.minute + now.second / 60;
-      Timer(Duration(seconds: 5), _calculateTime);
-    });
-  }
+  // void _calculateTime() {
+  //   DateTime now = DateTime.now();
+  //   setState(() {
+  //     _hours = now.hour + now.minute / 60;
+  //     _minutes = now.minute + now.second / 60;
+  //     Timer(Duration(seconds: 5), _calculateTime);
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -46,75 +51,93 @@ class _ThuisSchermState extends State<ThuisScherm> {
           child: Center(
             child: Padding(
               padding: const EdgeInsets.all(20.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Tweedag, 10.5',
-                    style: TextStyle(
-                      fontFamily: 'Gentium',
-                      fontSize: 30.0,
-                    ),
-                  ),
-                  Divider(
-                    thickness: 2.0,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(20.0),
-                    child: Stack(
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                              image: AssetImage('assets/images/clock1.png'),
-                              fit: BoxFit.contain,
-                            ),
-                          ),
-                          height: 200,
-                        ),
-                        Container(
-                          transform: Matrix4.rotationZ(_hours * math.pi / 6),
-                          transformAlignment: FractionalOffset.center,
-                          decoration: const BoxDecoration(
-                            image: DecorationImage(
-                                image: AssetImage('assets/images/hour.png'),
-                                fit: BoxFit.contain),
-                          ),
-                          height: 200,
-                        ),
-                        Container(
-                          transform: Matrix4.rotationZ(_minutes * math.pi / 30),
-                          transformAlignment: FractionalOffset.center,
-                          decoration: const BoxDecoration(
-                            image: DecorationImage(
-                              image: AssetImage('assets/images/minute1.png'),
-                              fit: BoxFit.contain,
-                            ),
-                          ),
-                          height: 200,
-                        ),
-                      ],
-                    ),
-                  ),
-                  Divider(
-                    thickness: 2.0,
-                  ),
-                  Text(
-                    'Moeharram 1444',
-                    style: TextStyle(
-                      fontFamily: 'Gentium',
-                      fontSize: 30.0,
-                    ),
-                  ),
-                  Text(
-                    'Middaggebed',
-                    style: TextStyle(
-                      fontFamily: 'Gentium',
-                      fontSize: 40.0,
-                    ),
-                  ),
-                ],
-              ),
+              child: StreamBuilder<Zonnetijd>(
+                  stream: kalender.krijgZonnetijdStroom(),
+                  builder: (context, snapshot) {
+                    print(snapshot.data?.uur);
+                    return !snapshot.hasData
+                        ? Center(
+                            child: CircularProgressIndicator(),
+                          )
+                        : Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                '${talen['nederlands']['weekdagen'][snapshot.data!.weekdag]}, ${snapshot.data!.dag}.${snapshot.data!.joodseMaand}',
+                                style: TextStyle(
+                                  fontFamily: 'Gentium',
+                                  fontSize: 30.0,
+                                ),
+                              ),
+                              Divider(
+                                thickness: 2.0,
+                              ),
+                              Padding(
+                                padding: EdgeInsets.all(20.0),
+                                child: Stack(
+                                  children: [
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                          image: AssetImage(
+                                              'assets/images/clock1.png'),
+                                          fit: BoxFit.contain,
+                                        ),
+                                      ),
+                                      height: 200,
+                                    ),
+                                    Container(
+                                      transform: Matrix4.rotationZ(
+                                          snapshot.data!.uur * math.pi / 6),
+                                      transformAlignment:
+                                          FractionalOffset.center,
+                                      decoration: const BoxDecoration(
+                                        image: DecorationImage(
+                                            image: AssetImage(
+                                                'assets/images/hour.png'),
+                                            fit: BoxFit.contain),
+                                      ),
+                                      height: 200,
+                                    ),
+                                    Container(
+                                      transform: Matrix4.rotationZ(
+                                          snapshot.data!.uur * math.pi * 2),
+                                      transformAlignment:
+                                          FractionalOffset.center,
+                                      decoration: const BoxDecoration(
+                                        image: DecorationImage(
+                                          image: AssetImage(
+                                              'assets/images/minute1.png'),
+                                          fit: BoxFit.contain,
+                                        ),
+                                      ),
+                                      height: 200,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Divider(
+                                thickness: 2.0,
+                              ),
+                              Text(
+                                '${talen['nederlands']['maanden'][snapshot.data?.hidjriMaand ?? 0]} ${snapshot.data?.hidjriJaar ?? 0}',
+                                style: TextStyle(
+                                  fontFamily: 'Gentium',
+                                  fontSize: 30.0,
+                                ),
+                              ),
+                              Text(
+                                talen['nederlands']['groeten'][
+                                    tijden.indexWhere((element) =>
+                                        element <= (snapshot.data?.uur ?? 0))],
+                                style: TextStyle(
+                                  fontFamily: 'Gentium',
+                                  fontSize: 40.0,
+                                ),
+                              ),
+                            ],
+                          );
+                  }),
             ),
           ),
         ),
